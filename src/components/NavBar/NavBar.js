97e4941c81921f612b1dd390e4cc3ee4f3709e27
Danylo <bodnar.dp@gmail.com>
2023-12-07 23:39:39 +0200

    nabvar_created

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,17 @@
+import {Component} from 'react'
+
+class NavBar extends Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(
+            <div className='NavBar'>
+                <h1>navigation</h1>
+            </div>
+        );
+    }
+}
+
+export default NavBar
---
+++ src/components/app/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+import NavBar from '../NavBar/NavBar';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar></NavBar>
+        <h1>This is Medical System</h1>
+    </div>
+  );
+}
+
+export default App;
