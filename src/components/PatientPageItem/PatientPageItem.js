a830d5afaeeebb384d0fa7ef0c1eadfb912915f8
Danylo <bodnar.dp@gmail.com>
2023-12-13 11:41:31 +0200

    add-record-modal-implemented

--- src/components/PatientPageItem/PatientPageItem.js
+++ src/components/PatientPageItem/PatientPageItem.js
@@ -4,9 +4,25 @@ import './PatientPageItem.css'
 
 import PatientCard from './../PatientCard/PatientCard';
 import RecordList from './../RecordList/RecordList';
+import AddRecord from './../AddRecord/AddRecord';
 
 class PatientPageItem extends Component{
+    constructor() {
+        super();
+        this.state = {
+            show: false
+        };
+        this.showModal = this.showModal.bind(this);
+        this.hideModal = this.hideModal.bind(this);
+    }
 
+    showModal = () => {
+        this.setState({ show: true });
+    };
+
+    hideModal = () => {
+        this.setState({ show: false });
+    };
 
     render(){
         const {patientID} = this.props;
@@ -14,7 +30,10 @@ class PatientPageItem extends Component{
             <div className="medical-card-page">
                 <div className="patient-card-container"> 
                     <PatientCard id={patientID}/> 
-                    <button className="add-form-button">Add new record</button>
+                    <AddRecord show={this.state.show} handleClose={this.hideModal}>
+                        <p>Modal</p>
+                    </AddRecord>
+                    <button className="add-form-button" type="button" onClick={this.showModal}>Add new record</button>
                 </div>
                 <div className="record-list-container">
                     <RecordList id={patientID}></RecordList>
