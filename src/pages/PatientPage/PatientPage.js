fedf8c2ff28956de8e38291ba934e7805ab1238a
Danylo <bodnar.dp@gmail.com>
2023-12-13 11:32:51 +0200

    patient-page-item-developed-as-class

--- src/pages/PatientPage/PatientPage.js
+++ src/pages/PatientPage/PatientPage.js
@@ -1,23 +1,11 @@
 import {useParams} from 'react-router-dom'
 
-import PatientCard from '../../components/PatientCard/PatientCard';
-import RecordList from '../../components/RecordList/RecordList';
-
-import './PatientPage.css';
+import PatientPageItem from '../../components/PatientPageItem/PatientPageItem';
 
 function PatientPage(){
     const { patientID } = useParams();
-
     return(
-        <div className="medical-card-page">
-            <div className="patient-card-container"> 
-                <PatientCard id={patientID}/> 
-                <button className="add-form-button">Add new record</button>
-            </div>
-            <div className="record-list-container">
-                <RecordList id={patientID}></RecordList>
-            </div>
-        </div>
+        <PatientPageItem patientID={patientID}/>
     );
 }
 
