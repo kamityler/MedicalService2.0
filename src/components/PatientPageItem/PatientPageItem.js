312069c6e7651b9c155e3da155dca7630c8893a9
Danylo <bodnar.dp@gmail.com>
2023-12-17 23:18:35 +0200

    changes

--- src/components/PatientPageItem/PatientPageItem.js
+++ src/components/PatientPageItem/PatientPageItem.js
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import axios from 'axios';
 
 import './PatientPageItem.css'
 
@@ -7,13 +8,31 @@ import RecordList from './../RecordList/RecordList';
 import ModalWindow from './../ModalWindow/ModalWindow';
 
 class PatientPageItem extends Component{
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
-            show: false
+            show: false,
+            medicalRecord: {
+                patientID: this.props.patientID,
+                diagnosis: 'some diagnosis',
+                appointmentDate: new Date(),
+                doctor: 'dr.Bodnar',
+                description: 'something went wrong',
+                treatment: 'two pills in the vessel',
+                appointmentType: 'General'
+            }
         };
         this.showModal = this.showModal.bind(this);
         this.hideModal = this.hideModal.bind(this);
+    }
+
+    addRecord = (e) => {
+        e.preventDefault();
+        axios.post(
+            `https://localhost:5001/api/MedicalRecords/${55}/Appointments`,
+            // headers: { 'Content-Type': 'application/json' },
+            {data : this.state.medicalRecord}
+        ).then(response => console.log(response)).catch(err => console.log(err))
     }
 
     showModal = () => {
@@ -31,7 +50,7 @@ class PatientPageItem extends Component{
                 <div className="patient-card-container"> 
                     <PatientCard id={patientID}/> 
                     <ModalWindow show={this.state.show} handleClose={this.hideModal}>
-                        <form className="modal-form">
+                        <form className="modal-form" onSubmit={this.addRecord}>
                             <h1 className="modal-header">Add new medical record</h1>
                             <p className="modal-label">Enter diagnosis here:</p>
                             <input required placeholder="Diagnosis" type="text" name="diagnosis" className="modal-field modal-input"></input>
@@ -40,7 +59,7 @@ class PatientPageItem extends Component{
                             <p className="modal-label">Enter ways of treatment here:</p>
                             <textarea required placeholder="Enter treatment here..." name="treatment" className="modal-field modal-textarea"></textarea>
                             <div className="form-button-container">
-                                <input className="form-button-submit" type="submit" value="Submit"/>
+                                <button className="form-button-submit" value="Submit" type="submit">Submit</button>
                                 <div className="between-div-container"></div>
                                 <input className="form-button-submit" type="reset" value="Reset"/>
                             </div>
