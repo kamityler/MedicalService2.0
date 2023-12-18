fc5ec700dc8b0578b657da4fc95716a6b1a6c2c2
Danylo <bodnar.dp@gmail.com>
2023-12-18 13:10:26 +0200

    settings-page-done

--- src/pages/UserPage/UserPage.js
+++ src/pages/UserPage/UserPage.js
@@ -5,8 +5,23 @@ import './UserPage.css';
 class UserPage extends Component{
 
     render(){
+        const url = "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_640.png";
         return(
             <div className="account-settings">
+                <div className="settings-spacer">
+
+                </div>
+                <div className="doctor-card">
+                    <ul className="doctor-info-ul">
+                            <img className="doctor-avatar" src={url} alt="doctor" />
+                            <li className="doctor-info-li">Name: <span className="exact-information-item">dr.Bodnar</span></li>
+                            <li className="doctor-info-li">Gender: <span className="exact-information-item">Male</span></li>
+                            <li className="doctor-info-li">Date of brith: <span className="exact-information-item">30.05.2003</span></li>
+                            <li className="doctor-info-li">Address: <span className="exact-information-item">Lviv, Ukraine</span></li>
+                            <li className="doctor-info-li">Email: <span className="exact-information-item">bodnar.dp@gmail.com</span></li>
+                            <li className="doctor-info-li">Phone: <span className="exact-information-item">+380731231234</span></li>
+                        </ul>
+                </div>
                 <div className="profile-container">
                     <div className="profile-header">
                         <h2>Profile Settings</h2>
@@ -14,18 +29,24 @@ class UserPage extends Component{
                     <div className="profile-form">
                         <form>
                             <div className="form-group">
-                                <label htmlFor="fullName">Your name: dr.Bodnar</label>
                                 <label htmlFor="fullName">Change your name:</label>
                                 <input type="text" id="fullName" name="fullName"/>
                             </div>
                             <div className="form-group">
-                                <label htmlFor="email">Your email: danylo.bodnar.pz.2020@lpnu.ua</label>
+                                <label htmlFor="adress">Change your adress:</label>
+                                <input type="text" id="adress" name="adress"/>
+                            </div>
+                            <div className="form-group">
                                 <label htmlFor="email">Change your email:</label>
                                 <input type="email" id="email" name="email" required/>
                             </div>
                             <div className="form-group">
+                                <label htmlFor="tel">Change your phone number:</label>
+                                <input type="text" id="tel" name="tel" required/>
+                            </div>
+                            <div className="form-group">
                                 <label htmlFor="password">New Password:</label>
-                                <input type="password" id="password" name="password"/>
+                                <input type="password" id="password" name="password" autoComplete="on"/>
                             </div>
                             <div className="form-group">
                                 <button type="submit" onClick={(e)=>{e.preventDefault()}}>Save Changes</button>
