9800707194365a6ee6b46ad7218e547e4b9a9464
RuslanMudruj <13072003.rm@gmail.com>
2023-12-18 15:42:41 +0200

    added main mage shit

--- src/pages/MainPage/Main.js
+++ src/pages/MainPage/Main.js
@@ -1,8 +1,35 @@
+import './Main.css';
+import axios from 'axios';
+
+
 function MainPage() {
+  
     return (
+      
       <div className="main-page">
-        <h2>Main page</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adip.</p>
+        <div className='news-contsiner'>
+          <article className='article-container'>
+            <h2>Advancing Medical Expertise: New Continuing Education Courses Empower Physicians</h2>
+            <p>In a dynamic era of healthcare, continuous learning is paramount for medical professionals to stay abreast of advancements and deliver optimal patient care. Recognizing this need, a comprehensive set of continuing education courses tailored for physicians has been introduced, aiming to enhance their skills and knowledge.</p>
+            <p>These specialized courses cover a wide array of medical disciplines, ranging from cutting-edge treatment modalities to the latest technological innovations in healthcare. Participants have the opportunity to delve into interactive sessions, workshops, and hands-on training, fostering a collaborative environment conducive to skill development.</p>
+            <p>As the field of medicine continues to evolve, investing in continuous education becomes imperative. These advanced courses not only empower physicians with the latest knowledge and skills but also contribute to a collaborative and forward-thinking medical community. By embracing lifelong learning, healthcare professionals are better positioned to navigate the complexities of modern medicine and, ultimately, provide superior care to their patients.</p>
+          </article>
+            
+               
+        </div>
+        <div className="second-container">
+          <article className='article-container'>
+            <h2>Statistics for today</h2>
+            <p>Doctors quantity: 1</p>
+            <p>Patients quantity: 14</p>
+          </article>  
+          <article className='article-container'>
+            <h2>Statistics for yesterday</h2>
+            <p>Doctors quantity: 1</p>
+            <p>Patients quantity: 13</p>
+          </article>  
+          
+        </div>
       </div>
     );
 }
