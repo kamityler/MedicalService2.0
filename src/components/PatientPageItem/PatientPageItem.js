8fa5601d3dcb1154d1a571632331be4efb6ffc05
RuslanMudruj <13072003.rm@gmail.com>
2023-12-22 10:05:35 +0200

    added change

--- src/components/PatientPageItem/PatientPageItem.js
+++ src/components/PatientPageItem/PatientPageItem.js
@@ -5,8 +5,7 @@ import './PatientPageItem.css'
 
 import PatientCard from './../PatientCard/PatientCard';
 import RecordList from './../RecordList/RecordList';
-import ModalWindow from "./../modalWindow/ModalWindow";
-
+import ModalWindow from './../modalWindow/ModalWindow'
 class PatientPageItem extends Component{
     constructor(props) {
         super(props);
